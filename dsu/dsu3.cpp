58f63b38791f014e4420e7a1b514a4237e755f99
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-06 23:39:05 +0530

    daily practise solution

---
+++ dsu/dsu3.cpp
@@ -0,0 +1,82 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findparent(int a,vector<int> &parent)
+{
+	if(parent[a]==a) return a;
+	return parent[a]=findparent(parent[a],parent);
+}
+
+void teammerge(int a,int b,vector<int> &parent,vector<int> &size,vector<int> &strength)
+{
+	int super_parent_a=findparent(a,parent);
+	int super_parent_b=findparent(b,parent);
+	if(super_parent_a==super_parent_b) return;
+	if(size[super_parent_a]<size[super_parent_b])
+	swap(super_parent_a,super_parent_b);
+	size[super_parent_a]+=size[super_parent_b];
+	strength[super_parent_a]+=strength[super_parent_b];
+	parent[super_parent_b]=super_parent_a;
+}
+
+void individualmerge(int a,int b,vector<int>&parent,vector<int>&size,vector<int>&strength)
+{
+	int super_parent_a=findparent(a,parent);
+	int super_parent_b=findparent(b,parent);
+	if(super_parent_a==super_parent_b) return;
+	size[super_parent_b]+=1;
+	strength[super_parent_b]+=a;
+	parent[a]=super_parent_b;
+	size[super_parent_a]-=1;
+	strength[super_parent_a]-=a;
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,q;
+		cin>>n>>q;
+		vector<int> team(2*n+1);
+		vector<int> size(2*n+1,1);
+		vector<int> parent(2*n+1);
+		vector<int> strength(2*n+1);
+		for(int i=1;i<=n;i++)
+		{
+			parent[i+n]=i+n;
+			parent[i]=i+n;
+			strength[i]=i;
+			strength[i+n]=i;
+			size[i]=1;
+			size[i+n]=1;
+		}
+		while(q--)
+		{
+			int qt;
+			cin>>qt;
+			if(qt==1)
+			{
+				int a,b;
+				cin>>a>>b;
+				if(findparent(a,parent)==findparent(b,parent)) continue;
+				teammerge(a,b,parent,size,strength);
+			}
+			if(qt==2)
+			{
+				int a;
+				cin>>a;
+				int parenta=findparent(a,parent);
+				cout<<size[parenta]<<" "<<strength[parenta]<<"\n";
+			}
+			if(qt==3)
+			{
+				int a,b;
+				cin>>a>>b;
+				individualmerge(a,b,parent,size,strength);
+			}
+		}
+	}
+}
