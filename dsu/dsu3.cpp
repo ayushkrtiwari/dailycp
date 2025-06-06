3db3dd528117efa38e49122ee6ae0d553edf439a
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-06 23:42:07 +0530

    2nd layer, individual and group merge

--- dsu/dsu3.cpp
+++ dsu/dsu3.cpp
@@ -1,3 +1,5 @@
+// जय श्री राम                              JAI SHREE RAM
+
 // https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/team-up-2-8dc5d882/
 
 // 2nd layer, individual and group merge
