bb91c429a69a9dfd7a420463f3ea1c66d77e14b6
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-06 23:39:38 +0530

    Question link

--- dsu/dsu14.cpp
+++ dsu/dsu14.cpp
@@ -1,4 +1,4 @@
-// // जय श्री राम                              JAI SHREE RAM
+// जय श्री राम                              JAI SHREE RAM
 
 // https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/a-69-5718e916/
 
