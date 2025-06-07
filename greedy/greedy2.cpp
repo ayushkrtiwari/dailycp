2736ce372b6fd6375800f5d5ad2f0c3df4df92f3
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-07 06:02:27 +0530

    daily practise

---
+++ greedy/greedy2.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/problemset/problem/22/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    vector<pair<int,int>> seg(n);
+    for(auto &z: seg)
+    {
+        int x,y;
+        cin>>x>>y;
+        z.first=min(x,y);
+        z.second=max(x,y);
+    }
+    sort(seg.begin(),seg.end(),[](const pair<int,int>&A, const pair<int,int>&B){return A.second<B.second;});
+    int i=0;
+    int cnt=0;
+    vector<int> nails;
+    while(i<n)
+    {
+        int take=seg[i].second;
+        cnt++;
+        nails.push_back(take);
+        while(take<=seg[i].second and take>=seg[i].first)
+        i++;
+    }
+    cout<<cnt<<"\n";
+    for(auto i:nails)
+    cout<<i<<"\n";
+}
