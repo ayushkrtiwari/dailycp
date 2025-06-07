f1ee99069b40e58b8cb3a6f57e0697c4c6348456
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-08 01:01:26 +0530

    Exchange Argument, Sorting, Expression Evaluation

--- greedy/greedy7.cpp
+++ greedy/greedy7.cpp
@@ -1,4 +1,11 @@
+// जय श्री राम                              JAI SHREE RAM
+
 // https://codeforces.com/contest/632/problem/C
+
+// Exchange Argument, Sorting
+// Trick: To sort lexicographically the string vector,
+// just check if my first ordering is better than second.
+// A + B < B + A -> return order A,B else B,A
 
 #include<bits/stdc++.h>
 using namespace std;
