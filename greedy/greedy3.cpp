0de6d31c7ff886a5d1ffadf13b2c1018925fb6c3
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-07 06:46:05 +0530

    Implementation

--- greedy/greedy3.cpp
+++ greedy/greedy3.cpp
@@ -11,19 +11,21 @@ int main()
     vector<pair<int,int>> xh(n);
     for(auto &x:xh)
     cin>>x.first>>x.second;
-    vector<int> left;
-    left.push_back(0);
+    vector<int> left; // stores left fall limit
+    left.push_back(0); // so that left.back() does not give error also x,h>0
+    // 1st tree always falls to left
+    // no left and right bounded here
     int cnt=0;
     if(n==1)
     {
-        cnt = 1;
+        cnt = 1; // when only tree, it can fall any side
     }
-    else
+    else // try if left fall possible else if right fall possible else no fall
     {
-        cnt = 1;
+        cnt = 1; // left tree always falls to left
         for(int i=1;i<n-1;i++)
         {
-            if(xh[i].first - xh[i].second > xh[i-1].first and xh[i].first - xh[i].second > left.back())
+            if(xh[i].first - xh[i].second > xh[i-1].first and xh[i].first - xh[i].second > left.back()) 
             {
                 cnt++;
             }
@@ -33,7 +35,7 @@ int main()
                 cnt++;
             }
         }
-        cnt++;
+        cnt++; // right tree can always fall to right
     }
     cout<<cnt<<"\n";
 }
