57227f8520a241dd9a34e06926159185a5fdda6c
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-07 15:48:18 +0530

    Solution added

--- greedy/greedy1.cpp
+++ greedy/greedy1.cpp
@@ -2,3 +2,29 @@
 
 // https://leetcode.com/problems/stone-game-vi/description/
 
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        long long sumalice=0,sumbob=0;
+        int n=bobValues.size();
+        vector<pair<int,int>> group(n);
+        for(int i=0;i<n;i++)
+        group[i].first=aliceValues[i],group[i].second=bobValues[i];
+        sort(group.begin(),group.end(),[](const pair<int,int>&A,const pair<int,int>&B)
+        {
+            return A.first+A.second>B.first+B.second;
+        });
+        for(int i=0;i<n;i++)
+        if(i&1)
+        {
+            sumbob+=group[i].second;
+        }
+        else
+        {
+            sumalice+=group[i].first;
+        }
+        if(sumalice>sumbob) return 1;
+        if(sumalice<sumbob) return -1;
+        return 0;
+    }
+};
