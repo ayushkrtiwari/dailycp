ab092db2d603d2f3c4c1c8b6621df3a561e50e12
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-07 16:07:42 +0530

    Exchange Argument, Sorting

--- greedy/greedy6.cpp
+++ greedy/greedy6.cpp
@@ -1,4 +1,11 @@
+// जय श्री राम                              JAI SHREE RAM
+
 // https://atcoder.jp/contests/abc131/tasks/abc131_d
+
+// Exchange Argument, Sorting
+
+// Trick: Sort by deadline, traverse each task, sum time each interval and check if it could fit the deadline
+// Exchange Argument are the greedy approach where expressions or sortings are used
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -9,7 +16,7 @@ int main()
     vector<pair<int,int>> ab(n);
     for(int i=0;i<n;i++)
     {
-        cin>>ab[i].first>>ab[i].second;
+        cin>>ab[i].first>>ab[i].second; // task time, deadline
     }
     sort(ab.begin(),ab.end(),[](const pair<int,int>&A,const pair<int,int>&B)
     {
