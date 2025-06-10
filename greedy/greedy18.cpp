74faaf7c8753e0b8513ea7064cef0cc4956efb2e
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-10 23:18:31 +0530

    Daily practice problem

---
+++ greedy/greedy18.cpp
@@ -0,0 +1,29 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>> AR(n);
+    for(auto &x:AR)
+    cin>>x.first>>x.second;
+    vector<vector<int>> deadlineday(m);
+    for(auto x: AR)
+    if (m >= x.first)
+    deadlineday[m - x.first].push_back(x.second);
+    priority_queue<int> pq;
+    long long totalreward = 0;
+    for(int i = m - 1; i >= 0; i--)
+    {
+        for(auto rew:deadlineday[i])
+        pq.push(rew);
+        if(!pq.empty())
+        {
+        totalreward += pq.top();
+        pq.pop();
+        }
+    }
+    cout<<totalreward<<"\n";
+}
