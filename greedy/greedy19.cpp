4f546784a8bd2d1042ae081701c3428b8fe5966e
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-11 23:56:56 +0530

    Trick updated

--- greedy/greedy19.cpp
+++ greedy/greedy19.cpp
@@ -1,6 +1,7 @@
 // https://maang.in/problems/Diversify-the-Array-97?returnUrl=%5B%22%2Fplaylists%2FSummer-Camp-S2-Week-3-Greedy-4868%3FresourceUrl%3Dcs181-cp795-pl4868-rs11623%26returnUrl%3D%255B%2522%252Fcourses%252FICPSC-S2-181%253Ftab%253Dchapters%2522%255D%22%5D
 
 // Trick: Decrease count of largest repeating element first
+// Also note that decrease 1 count at a time and push count - 1 back into max heap. Dont decrease whole count at a time
 
 #include<bits/stdc++.h>
 # define int long long
