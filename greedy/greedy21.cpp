a8d43b5d1c6d02d6a850ea9affc47b7e4d956ff5
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-17 23:42:52 +0530

    daily practice solution

---
+++ greedy/greedy21.cpp
@@ -0,0 +1,49 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<char>> v(2,vector<char>(n));
+        for(int i = 0; i < 2; i++)
+        for(int j = 0; j < n; j++)
+        cin>>v[i][j];
+        bool ispossible = true;
+        bool ismultiple = false;
+        int i;
+        for(i = 0; i < n - 1; i++)
+        {
+            if(v[0][i] == '.' and v[0][i] == v[1][i] and v[0][i + 1] == '.' and v[0][i + 1] == v[1][i + 1])
+            ismultiple = true;
+            if(v[0][i] == '.' and v[0][i] == v[1][i])
+            v[0][i] = '#', v[1][i] = '#';
+            if(v[0][i] == '.' and v[0][i + 1] == '.')
+            v[0][i] = '#', v[0][i + 1] = '#';
+            if(v[1][i] == '.' and v[1][i + 1] == '.')
+            v[1][i] = '#', v[1][i + 1] = '#';
+        }
+        if(v[0][i] == v[1][i]) v[0][i] = '#', v[1][i] = '#';
+        for(int i = 0; i < 2; i++)
+        for(int j = 0; j < n; j++)
+        if(v[i][j] == '.')
+        {
+            ispossible = false;
+            break;
+        }
+        if(!ispossible)
+        cout<<"None\n";
+        else
+        {
+            if(ismultiple)
+            cout<<"Multiple\n";
+            else
+            cout<<"Unique\n";
+        }
+    }
+}
