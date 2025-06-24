4a6c2f2a23db65f4a0ba66e2c2db39539795319b
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-24 23:32:50 +0530

    notes added

--- binarylifting.cpp/binlift3.cpp
+++ binarylifting.cpp/binlift3.cpp
@@ -86,7 +86,14 @@ int32_t main() {
         }
         binlift[n][0] = n;
         for(int j = 1; j < LOG; j++)
-        for(int i = 0; i <= n; i++) 
+        for(int i = 0; i <= n; i++) // done for i == n too since if n jumps to n only according
+                        // to above prefix binary search method and practically too, since
+                        // when we are at n and we have k = 3, then the value of possible k 
+                        // can be 0, 1, 2, 3 jumps and so we can do 0 jump on each binlift[n][j]
+                        // since we want max of n - i and if we dont do such, we get -1 - i
+                        // which will be wrong since even at no jump we are at max n, so we do
+                        // assign n for each jump from n
+
         if(binlift[i][j - 1] != -1)
         binlift[i][j] = binlift[binlift[i][j - 1]][j - 1];
         
