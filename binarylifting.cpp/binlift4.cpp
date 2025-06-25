9b07f6a223992b6d6d5b6b57ddac44c53936cc4f
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-25 23:23:21 +0530

    Trick added

--- binarylifting.cpp/binlift4.cpp
+++ binarylifting.cpp/binlift4.cpp
@@ -1,5 +1,8 @@
 // https://codeforces.com/contest/1142/problem/B
 
+// Not a cup of tea! Follow throughout the process
+// We could think of a naive solution, and it would get TLE
+
 
 #include <bits/stdc++.h>
 using namespace std;
