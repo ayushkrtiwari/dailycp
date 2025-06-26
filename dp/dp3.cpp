dd49affab39797fe3952f941f24ef6bea08fc380
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-26 23:44:46 +0530

    notes added

--- dp/dp3.cpp
+++ dp/dp3.cpp
@@ -12,6 +12,7 @@ int main()
     vector<int> a(n);
     for(auto &x:a) cin>>x;
     for(auto &x:a) x = x % 2;
+    // storing all subsequence of length 1 with parity 0, 1
     vector<int> sbsqncl1(2, 0); 
     vector<vector<int>> sbsqncl2(2, vector<int>(2, 0));
     sbsqncl1[a[0]]++;
@@ -22,6 +23,14 @@ int main()
     for(int i = 2; i < n; i++)
     {
         auto old = sbsqncl2;
+        // we store this since sbsqncl2 will change while operations,
+        // but we need old values
+        
+        // pairing: old    sbsqncl2  --> sbsqnce of length > 2
+        //           01      11      -->       011
+        //           00      00      -->       000
+        //           10      01      -->       101
+        //           11      10      -->       110
         for(int u = 0; u < 2; u++)
         {
             for(int v = 0; v < 2; v++)
