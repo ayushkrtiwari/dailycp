1b4257bd6a285532af6ab307a3779758859d1047
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-26 23:43:25 +0530

    problem statement explained

--- dp/dp3.cpp
+++ dp/dp3.cpp
@@ -1,3 +1,7 @@
 // https://codeforces.com/problemset/problem/1912/K
 
 
+/**
+ * The function calculates the count of subsequences of length greater than 2 with 3 continuous
+ * elements of even sum in a given array.
+ */
