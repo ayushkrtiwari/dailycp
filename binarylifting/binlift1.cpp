1b249fd27cd9982c0286d68b367023b25d032596
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-28 23:52:55 +0530

    daily practice solution

--- binarylifting/binlift1.cpp
+++ binarylifting/binlift1.cpp
@@ -1,1 +1,34 @@
 // https://cses.fi/problemset/task/1687
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,q;
+    cin>>n>>q;
+    int col = (int)log2(n) + 1;
+    vector<vector<int>> binlift(n + 1, vector<int>(col, -1));
+    for(int i = 2; i <= n; i++)
+    cin>>binlift[i][0];
+    for(int c = 1; c < col; c++)
+    for(int i = 2; i <= n; i++)
+    if(binlift[i][c - 1] != -1 and binlift[binlift[i][c - 1]][c - 1])
+    binlift[i][c] = binlift[binlift[i][c - 1]][c - 1];
+    while(q--)
+    {
+        int emp, k;
+        cin>>emp>>k;
+        int x = col;
+        // int ans = -1;
+        for(int i = 0; i <= x; i++)
+        if(k & (1<<i))
+        if(binlift[emp][i] == -1) 
+        {
+            emp = -1;
+            break;
+        }
+        else
+        emp = binlift[emp][i];
+        cout<<emp<<"\n";
+    }
+}
