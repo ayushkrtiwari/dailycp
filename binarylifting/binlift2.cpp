fd5ab7857813367aed52b6de43141175cf6b32b7
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-06-30 23:51:46 +0530

    Topic added

--- binarylifting/binlift2.cpp
+++ binarylifting/binlift2.cpp
@@ -2,6 +2,7 @@
 
 // https://leetcode.com/problems/kth-ancestor-of-a-tree-node/
 
+// Topic: Binary Lifting, Tree
 // Trick: Just use binary lifting to pass constraints on finding kth ancestor
 
 #pragma GCC optimize("O3,unroll-loops,Ofast")
