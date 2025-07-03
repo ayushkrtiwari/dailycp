1f7d27e92c38630c7c154f12053ade6817a82a0e
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-04 00:03:46 +0530

    Trick Updated

--- binarylifting/binlift4.cpp
+++ binarylifting/binlift4.cpp
@@ -2,7 +2,9 @@
 
 // https://www.codechef.com/problems/LGSEG?tab=statement
 
-
+// Trick: Just build next vector which stores next node to reach with 1 jump
+// Use binary search on prefix array better than 2 pointer
+// Use binary lifting to avoid TLE by storing next node on k jumps
 
 #include <bits/stdc++.h>
 using namespace std;
