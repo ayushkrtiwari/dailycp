371ae96859aca7bf338e098970022634495020f0
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-08 23:46:37 +0530

    daily practice solution

--- binarylifting/binlift5.cpp
+++ binarylifting/binlift5.cpp
@@ -1,3 +1,85 @@
 
 // https://codeforces.com/contest/1142/problem/B
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n,m,q;
+    cin>>n>>m>>q;
+    vector<int> p(n);
+    vector<int> posinperm(n, -1);
+    for(int i = 0; i < n; i++) 
+    {
+        cin>>p[i];
+        p[i]--;
+        posinperm[p[i]] = i;
+    }
+    vector<int> a(m);
+    vector<vector<int>> pos(n);
+    for(int i = 0; i < m; i++) 
+    {
+        cin>>a[i];
+        a[i]--;
+        pos[a[i]].push_back(i);
+    }
+    vector<int> nxt(m);
+    for(int i = 0; i < m; i++)
+    {
+        int npe = p[(posinperm[a[i]] + 1) % n];
+        auto it = lower_bound(pos[npe].begin(), pos[npe].end(), i + 1); 
+        if(it == pos[npe].end()) nxt[i] = m;
+        else nxt[i] = *it;
+    }
+    int LOG = (int)log2(m) + 1;
+    vector<vector<int>> binjump(m + 1, vector<int>(LOG, m));
+    for(int i = 0; i < m; i++)
+    binjump[i][0] = nxt[i];
+    
+    for(int j = 1; j < LOG; j++)
+    for(int i = 0; i <= m; i++)
+    binjump[i][j] = binjump[binjump[i][j - 1]][j - 1];
+    
+    int totaljumps = n - 1;
+    vector<int> reached(m);
+    for(int i = 0; i < m; i++)
+    {
+        int atpos = i;
+        for(int j = 0; j < LOG; j++)
+        {
+            if((totaljumps>>j) & 1)
+            {
+                atpos = binjump[atpos][j];
+            }
+        }
+        reached[i] = atpos;
+    }
+    
+    int k = (int)log2(m) + 1;
+    vector<vector<int>> sparse(k, vector<int>(m, -1));
+    for(int j = 0; j < m; j++)
+    sparse[0][j] = reached[j];
+    for(int i = 1; i < k; i++)
+    for(int j = 0; j + (1 << i) <= m; j++)
+    sparse[i][j] = min(sparse[i - 1][j], sparse[i - 1][j + (1 << (i - 1))]);
+
+    while(q--)
+    {
+        int l, r;
+        cin>>l>>r;
+        l--, r--;
+        if(n > m)
+        cout<<0;
+        else
+        {
+            int i = log2(r - l + 1);
+            int minimum = min(sparse[i][l], sparse[i][r - (1 << i) + 1]);
+            if(minimum <= r)
+            cout<<1;
+            else
+            cout<<0;
+        }
+    }
+    cout<<"\n";
+}
