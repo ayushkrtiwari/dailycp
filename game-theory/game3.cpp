9ff7bf011d194093da832db5e46bcceca739c474
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-24 23:43:56 +0530

    Trick Added

--- game-theory/game3.cpp
+++ game-theory/game3.cpp
@@ -1,6 +1,8 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/tower-breakers
 
 // Unbounded Normal Nim Game
+// Trick: Follow Resources and editorials for understanding.
+// Just it is that copy the opponent's move, thus nim sum or specific cases as always
 
 #include <cmath>
 #include <cstdio>
@@ -8,7 +10,6 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 
 int main() {
     int t;
