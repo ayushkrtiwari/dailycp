dbe0c6b42e4d5d1c6e0af60011182997d4ef2671
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-24 23:36:48 +0530

    daily practice solution

--- game-theory/game2.cpp
+++ game-theory/game2.cpp
@@ -1,2 +1,23 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/a-game-of-stones
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n % 7 == 0 or n % 7 == 1) cout<<"Second\n";
+        else cout<<"First\n";
+    }
+    return 0;
+}
