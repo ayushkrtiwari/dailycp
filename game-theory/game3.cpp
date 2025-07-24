8678c1116cda0b5bbde1c9956a936aa55bd97d22
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-24 23:39:52 +0530

    daily practice solution

--- game-theory/game3.cpp
+++ game-theory/game3.cpp
@@ -1,1 +1,24 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/tower-breakers
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        if(m == 1 or n % 2 == 0)
+            cout<<"2\n";
+        else
+            cout<<"1\n";
+    }
+    return 0;
+}
