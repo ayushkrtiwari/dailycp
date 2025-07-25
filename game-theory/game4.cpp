615a0170494588799bb96d728c8f0ed44f38a51e
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-25 23:56:34 +0530

    Notes added

--- game-theory/game4.cpp
+++ game-theory/game4.cpp
@@ -1,5 +1,7 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/day-1-a-chessboard-game/problem
 
+// Repetition on board, transition pattern
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
