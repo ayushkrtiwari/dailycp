24af3ce4ef8975acd11559b9109736cba3756d05
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-27 23:31:01 +0530

    daily practice solution

--- game-theory/game6.cpp
+++ game-theory/game6.cpp
@@ -1,2 +1,28 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/nimble
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> s(n);
+        for(auto &x:s) cin>>x;
+        int nim = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(s[i] & 1) nim ^= i;
+        }
+        if(nim) cout<<"First\n";
+        else cout<<"Second\n";
+    }
+    return 0;
+}
