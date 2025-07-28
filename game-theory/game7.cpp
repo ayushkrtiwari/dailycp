3be2cab641dda244825bff7ab7b00adeba5fc568
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-28 23:57:42 +0530

    Topic Added

--- game-theory/game7.cpp
+++ game-theory/game7.cpp
@@ -1,5 +1,7 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/day-2-poker-nim
 
+// Unbounded Normal Nim Game
+
 // Addition of chips has no effect since winning strategy is to copy the other player's move whenever they perform addition.
 // Hence addition does not affect our winning strategy and thus we can play it as normal nim game.
 
