a8f37f0691c794627283ed5b55ed2913b0081057
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-28 23:56:48 +0530

    daily practice solution

--- game-theory/game7.cpp
+++ game-theory/game7.cpp
@@ -1,2 +1,26 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/day-2-poker-nim
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> s(n);
+        for(auto &x:s) cin>>x;
+        int nim = 0;
+        for(auto &x:s) nim ^= x;
+        if(nim) cout<<"First\n";
+        else cout<<"Second\n";
+    }
+    return 0;
+}
