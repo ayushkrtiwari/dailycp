1b6a93366c79122f82df6bacf46e6dc89414e50a
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-07-30 23:46:39 +0530

    daily practice solution

--- game-theory/game9.cpp
+++ game-theory/game9.cpp
@@ -1,2 +1,60 @@
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/tower-breakers-3/problem
 
+#include <bits/stdc++.h>
+using namespace std;
+static constexpr int MAXN = 1e5 + 2;
+vector<int> grundy(MAXN);
+
+int findmex(set<int> &mex)
+{
+    int cnt = 0;
+    for(auto &x : mex)
+        if(cnt != x) return cnt;
+        else cnt++;
+    return cnt;
+}
+
+void precompute()
+{
+    grundy[0] = 0, grundy[1] = 0;
+    for(int i = 2; i < MAXN; i++)
+    {
+        // int xorg = 0;
+        set<int> mex;
+        for(int j = 2; j * j <= i; j++)
+        {
+            if(i % j) continue;
+            int div1 = j, div2 = i/j;
+            if(div1 % 2 and div2 % 2)
+            mex.insert(grundy[div1]), mex.insert(grundy[div2]);
+            else if(div1 % 2) mex.insert(grundy[div2]);
+            else if(div2 % 2) mex.insert(grundy[div1]);
+            else mex.insert(0);
+        }
+        mex.insert(0); // since when broken into parts of 1 then grundy will be 0
+        grundy[i] = findmex(mex);
+        mex.clear();
+    }
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    precompute();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> h(n);
+        for(auto &x:h) cin>>x;
+        // for(auto &x:h) x = grundy[x];
+        int xorg = 0;
+        for(auto &x: h) xorg ^= grundy[x];
+        if(xorg) cout<<"1\n";
+        else cout<<"2\n";
+    }
+    // for(int x = 0; x < 101; x++) cout<<grundy[x]<<" ";
+    return 0;
+}
+
