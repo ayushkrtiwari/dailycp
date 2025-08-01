98462bde522d82311d4cbf2635bb624d5ab34b4b
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-01 23:55:54 +0530

    Bounded Normal Nim Game, Brute Force, Sprague Grundy, Explore all states

--- game-theory/game11.cpp
+++ game-theory/game11.cpp
@@ -1,3 +1,5 @@
+// जय श्री राम  JAI SHREE RAM
+
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/digits-square-board/problem
 
 // Bounded Normal Nim Game, Brute Force, Sprague Grundy, Explore all states
