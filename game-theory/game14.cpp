2020e53cd68a077872e72f725dda2a2ffce0c1d2
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-03 19:06:26 +0530

    Green Hackenbush on General Rooted Graphs, Unbounded Nim Game

--- game-theory/game14.cpp
+++ game-theory/game14.cpp
@@ -1,3 +1,5 @@
+// जय श्री राम  JAI SHREE RAM
+
 // https://www.hackerrank.com/contests/5-days-of-game-theory/challenges/deforestation/problem
 
 // Green Hackenbush on General Rooted Graphs, Unbounded Nim Game
