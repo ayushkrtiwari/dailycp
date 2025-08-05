fd9a54bd99f9de8d41f7e571f2c2d94d04683424
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-05 23:55:09 +0530

    daily practice solution

--- game-theory/game18.cpp
+++ game-theory/game18.cpp
@@ -1,2 +1,25 @@
 // https://cses.fi/problemset/task/1098
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(auto &x : v)
+        {
+            cin>>x;
+            x %= 4;
+        }
+        int nim = 0;
+        for(auto &x : v)
+        nim ^= x;
+        if(nim) cout<<"first\n";
+        else cout<<"second\n";
+    }
+}
