c22f7cdc4d042c17fa16ded52243aa858e656be5
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-06 22:25:24 +0530

    daily practice solution

--- game-theory/game20.cpp
+++ game-theory/game20.cpp
@@ -1,1 +1,27 @@
 // https://cses.fi/problemset/task/1729
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    vector<int> p(k);
+    for(auto &x : p) cin>>x;
+    vector<int> winlose(n + 1, 0);
+    for(int i = 1; i <= n; i++)
+    {
+        for(int step = 0; step < k; step++)
+        {
+            if(i - p[step] >= 0 and winlose[i - p[step]] == 0)
+            {
+                winlose[i] = 1;
+                break;
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++)
+    if(winlose[i]) cout<<'W';
+    else cout<<'L';
+}
