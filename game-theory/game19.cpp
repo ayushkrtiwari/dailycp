268cdbb3d47c7591e8e66c1d40f76bc30b62a632
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-06 22:22:36 +0530

    daily practice solution

--- game-theory/game19.cpp
+++ game-theory/game19.cpp
@@ -1,2 +1,28 @@
 // https://cses.fi/problemset/task/1099
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> p(n);
+        for(auto &x : p)
+        {
+            cin>>x;
+            // x %= 2;
+        }
+        int nim = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(i % 2)
+            nim ^= p[i];
+        }
+        if(nim) cout<<"first\n";
+        else cout<<"second\n";
+    }
+}
