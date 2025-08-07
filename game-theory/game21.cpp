0862770bc8360764599b360dbd5b0bcd382cd29b
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-07 23:47:10 +0530

    notes added

--- game-theory/game21.cpp
+++ game-theory/game21.cpp
@@ -1,5 +1,9 @@
 // https://cses.fi/problemset/task/2208
 
+// If all x[i] are even, then every move subtracts 1 from some subset of heaps, 
+// turning each chosen heap from even→odd. But since you must choose at least one heap, 
+// you’ll always leave at least one odd heap for your opponent. 
+
 #include<bits/stdc++.h>
 using namespace std;
 
