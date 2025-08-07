992f7d490c1077a13399a999faff37736fcd9f37
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-07 23:31:37 +0530

    daily practice solution

--- game-theory/game21.cpp
+++ game-theory/game21.cpp
@@ -1,2 +1,26 @@
 // https://cses.fi/problemset/task/2208
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> x(n);
+        for(auto &e : x)
+        cin>>e;
+        bool firstwin = false;
+        for(auto &e : x)
+        if(e % 2)
+        {
+            firstwin = true;
+            break;
+        }
+        cout<<(firstwin ? "first\n" : "second\n");
+    }
+}
