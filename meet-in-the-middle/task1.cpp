09050ed373daf140ae25b96b476c4c17a416a716
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-09 23:51:45 +0530

    Topic Added

--- meet-in-the-middle/task1.cpp
+++ meet-in-the-middle/task1.cpp
@@ -1,5 +1,7 @@
 // https://lightoj.com/problem/funny-knapsack
 
+// Meet-in-the-middle, Binary Search, Bitmasking, Implementation
+
 // You need all the weights combination such that total weight <= W
 // You can brute force but it results in TLE : since 2 ^ 30 = 10 ^ 9 > 10 ^ 8
 // But we see that we can divide array into two parts from mid
