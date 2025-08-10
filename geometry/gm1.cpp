270bdfc25fb1ade9582639072057f653a7c520c0
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-10 23:36:03 +0530

    daily practice solution

--- geometry/gm1.cpp
+++ geometry/gm1.cpp
@@ -1,1 +1,26 @@
 // https://codeforces.com/group/DeilFl9Bhi/contest/329185/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template <class F>
+struct Point {
+    F x, y;
+    Point() : x(0), y(0) {}
+    Point(const F& x, const F& y) : x(x), y(y) {}
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<Point<long long>> p(n);
+    for(auto &e : p)
+    cin>>e.x>>e.y;
+    int posx = 0, negx = 0;
+    for(auto &e : p)
+    if(e.x > 0) posx++;
+    else negx++;
+    if(posx < 2 or negx < 2) cout<<"Yes";
+    else cout<<"No";
+}
