d2c8eaa8a71ccdecf18a70afa8ed1e24cdcc01e9
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-11 23:57:35 +0530

    daily practice solution

--- geometry/gm3.cpp
+++ geometry/gm3.cpp
@@ -1,1 +1,15 @@
 // https://codeforces.com/group/DeilFl9Bhi/contest/329185/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, c;
+        cin>>a>>b>>c;
+        cout<<max(a, max(b, c))<<"\n";
+    }
+}
