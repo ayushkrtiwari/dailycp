308133f9a0a00865d13d0b152bb26a57e7dac1d4
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-11 23:58:30 +0530

    daily practice solution

--- geometry/gm4.cpp
+++ geometry/gm4.cpp
@@ -1,1 +1,19 @@
 // https://codeforces.com/problemset/problem/712/C
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin>>x>>y;
+    int a[3] = {y, y, y};
+    int ans = 0;
+    while(a[0] != x or a[1] != x or a[2] != x)
+    {
+        sort(a, a + 3);
+        a[0] = min(x, a[1] + a[2] - 1);
+        ans++;
+        // cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<"\n";
+    }
+    cout<<ans<<"\n";
+}
