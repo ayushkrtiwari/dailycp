a55d11ee835bb31b491bcbfcba0f0ad5aaaf7284
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-12 23:56:10 +0530

    daily practice solution

--- geometry/gm5.cpp
+++ geometry/gm5.cpp
@@ -1,1 +1,24 @@
 // https://codeforces.com/group/DeilFl9Bhi/contest/329185/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    size_t n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &x:a) cin>>x;
+    if(a[0] + a[1] <= a[n - 1])
+    cout<<"1 2 "<<n<<"\n";
+    else
+    cout<<"-1\n";
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    solve();
+}
