fcaf62d7d48c8af0eef0e69d0d4cc9b5da78b2d8
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-12 23:56:31 +0530

    Notes Added

--- geometry/gm5.cpp
+++ geometry/gm5.cpp
@@ -3,6 +3,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// void solve()
+// {
+//     size_t n;
+//     cin>>n;
+//     vector<int> a(n);
+//     for(auto &x : a)
+//     cin>>x;
+//     int x = 0;
+//     for(auto z = --a.end(); z > a.begin(); z--)
+//     {
+//         int y = *z - a[x];
+//         auto it = upper_bound(a.begin() + 1, a.end(), y);
+//         if(it < z) 
+//         {
+//             cout<<a[x]<<" "<<*it<<" "<<*z<<"\n";
+//             return;
+//         }
+//     }
+//     cout<<-1<<"\n";
+//     return;
+// }
+
 void solve()
 {
     size_t n;
