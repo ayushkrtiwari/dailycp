df2b82b9d99479f7191d6a426380883a50b91ce7
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-13 23:49:03 +0530

    daily practice solution

--- geometry/gm6.cpp
+++ geometry/gm6.cpp
@@ -1,1 +1,20 @@
 // https://codeforces.com/group/DeilFl9Bhi/contest/329185/problem/E
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    size_t n;
+    cin>>n;
+    if(n % 4) cout<<"NO\n";
+    else cout<<"YES\n";
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    solve();
+}
