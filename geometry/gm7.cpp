ce34493ee4382ba0c95a6dd0dceea6ebcf360938
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-13 23:52:08 +0530

    daily practice solution

--- geometry/gm7.cpp
+++ geometry/gm7.cpp
@@ -1,1 +1,16 @@
 // https://codeforces.com/group/DeilFl9Bhi/contest/329185/problem/F
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        if(n % m) cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+}
