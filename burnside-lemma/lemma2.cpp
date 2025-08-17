c12f67ed4e8b349de8521c6e5365a0fc913828c0
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-17 23:14:17 +0530

    daily practice solution

--- burnside-lemma/lemma2.cpp
+++ burnside-lemma/lemma2.cpp
@@ -1,1 +1,35 @@
 // https://cses.fi/problemset/task/2209
+
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+static constexpr int MOD = 1e9 + 7;
+
+int binpower(int base, int e)
+{
+    int res = 1;
+    while(e > 0)
+    {
+        if(e & 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        e >>= 1;
+    }
+    return res;
+}
+
+int32_t main()
+{
+    int n, m;
+    cin>>n>>m;
+    int sum = 0;
+    int cnt = 0;
+    int gcd;
+    for(int i = 0; i < n; i++)
+    {
+        gcd = __gcd(i, n);
+        cnt = (cnt + gcd) % MOD;
+        sum = (sum + binpower(m, gcd)) % MOD;
+    }
+    sum = (sum * binpower(n, MOD - 2)) % MOD;
+    cout<<sum<<"\n";
+}
