aa0d389d0851db1a7cd48f0353fb30436afbb5c0
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-22 23:31:27 +0530

    Trick Updated

--- geometry/gmt6.cpp
+++ geometry/gmt6.cpp
@@ -1,6 +1,7 @@
 // https://open.kattis.com/problems/robotprotection
 
 // Robot covers the area formed by the convex hull of the polygon
+// Hence, convex hull area of the polygon is the solution
 
 #include<bits/stdc++.h>
 using namespace std;
