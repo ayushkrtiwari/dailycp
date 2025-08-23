1101a98539a4aee0a07cd09304cbe14736b2277d
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-23 23:51:52 +0530

    daily practice solution

--- sweepline/scanline1.cpp
+++ sweepline/scanline1.cpp
@@ -1,1 +1,25 @@
 // https://leetcode.com/problem-list/o1qf3c31/
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int n = trips.size();
+        vector<int> travel(1002,0);
+        for(int i=0;i<n;i++)
+        {
+            int x = trips[i][1];
+            int y = trips[i][2];
+            int cnt = trips[i][0];
+            travel[x]+=cnt;
+            travel[y]-=cnt;
+        }
+        int sum=0,mxsum=0;
+        for(int i=0;i<1002;i++)
+        {
+            sum+=travel[i];
+            if(sum>capacity) return false;
+            // mxsum=max(mxsum,sum);
+        }
+        return true;
+    }
+};
