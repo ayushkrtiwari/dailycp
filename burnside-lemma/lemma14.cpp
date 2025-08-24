ff5ad0f6cd2ccfc50cb1b11a367e51f059778f37
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-24 23:50:43 +0530

    Submission Judge Link Added

--- burnside-lemma/lemma14.cpp
+++ burnside-lemma/lemma14.cpp
@@ -1,1 +1,2 @@
 // https://codeforces.com/gym/101873/attachments/download/7413/20172018-acmicpc-german-collegiate-programming-contest-gcpc-2017-en.pdf
+// https://basecamp.eolymp.com/en/problems/11615
