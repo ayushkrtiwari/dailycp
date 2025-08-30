be42c64b2f10ad9b664cc3bc818a667e30834ec9
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-08-30 23:27:32 +0530

    Notes Updated

--- dsu_on_tree/dst1.cpp
+++ dsu_on_tree/dst1.cpp
@@ -13,6 +13,8 @@
 // So, we know that light edge can have only log2(n) parents/ancestors,
 // hence only those log2(n) ancestors when have dfs, then only that node contributes to cnt
 // So 1 light node contributes log2(n) times, hence n(say) nodes contribute n log n times.
+// For heavy nodes, each nodes add only constant time, since they are added only once and contribute only once to cnt
+// This way they have complexity of O(n) only.
 
 #include <bits/stdc++.h>
 using namespace std;
