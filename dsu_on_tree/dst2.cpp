233c77fab115c57336c283fe203feaa7bb6a3a32
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-09-03 23:46:45 +0530

    Notes Updated

--- dsu_on_tree/dst2.cpp
+++ dsu_on_tree/dst2.cpp
@@ -8,7 +8,7 @@
 // We need to get the query answer as : in subtree of node v(inclusive does not change solution, both are palindrome),
 // take all nodes at depth(1-indexed) and check if they could form palindrome
 // for this we get TLE on naive approach by checking each subtree of v at height h
-
+// Hence to optimise, we store all nodes at height h in nodesdeph[h] 2d vector
 #include<bits/stdc++.h>
 using namespace std;
 // # define int long long
